c913333c8eee02a5127add90ec05b1531fd51b26
natalia <nevidomaya.natalia@gmail.com>
2023-04-09 15:55:13 +0300

    Lesson9

---
+++ tasks/lesson10/task1.js
@@ -0,0 +1,17 @@
+
+// 10-1.
+// Напишіть код в /* Ваш код */, щоб він працював
+// let names = {
+//     first: "Tom",
+//     second: "Sam",
+//     third: "Ray",
+//     fourth: "Bob",
+// };
+
+// let {/* Ваш код */} = names;
+
+// console.log(f); // "Tom"
+
+// console.log(x); // "Ray"
+
+// console.log(fifth); // "Name №5"
