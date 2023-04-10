6dd208c46908768aea4d0c28e90793121b92bd70
natalia <nevidomaya.natalia@gmail.com>
2023-04-10 16:17:59 +0300

    l9

--- tasks/lesson9/tasks.js
+++ tasks/lesson9/tasks.js
@@ -1,5 +1,6 @@
 // 9-1.
-// Для заданої сторінки знайдіть всі <h2> з класом head, зробіть для них зелений колір фону, потім серед знайдених елементів знайдіть елементи з класом inner і поставте їм розмір шрифту 35px.
+// Для заданої сторінки знайдіть всі <h2> з класом head, зробіть для них зелений колір фону, потім серед знайдених елементів 
+// знайдіть елементи з класом inner і поставте їм розмір шрифту 35px.
 //     <h2 class="head">header1</h2>
 //     <h2 class="head">header2 <span class="inner">inner elem1</span></h2>
 //     <h2>header3</h2>
@@ -7,11 +8,9 @@
 //     <h2>header5</h2>
 
 $(document).ready(function () {
-    $(".head").css("background-color", "green");
-});
-
-$(document).ready(function () {
-    $(".inner").css("font-size", "35px");
+    const $h2 = $("h2.head")
+    $h2.css("background-color", "green");
+    $h2.find(".inner").css("font-size", "35px");
 });
 
 
